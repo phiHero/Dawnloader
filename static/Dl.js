ae98e615ba0ef4c63f733c7f63789c468757a123
Hung Hoang <H2PHero>
2021-11-26 12:08:11 +0700

    update 0.1

--- static/Dl.js
+++ static/Dl.js
@@ -2,6 +2,22 @@
 const inputBox = document.getElementById("input1");
 const Dbutton = document.getElementById("button1");
 
+Dbutton.disabled = true;
+Dbutton.onclick = () => {
+  setTimeout(function () {
+    inputBox.value = "";
+  }, 2000);
+};
+
+inputBox.onkeydown = () => {
+  let userData = inputBox.value;
+  if (userData.trim() != 0) {
+    Dbutton.classList.add("active_1");
+    Dbutton.disabled = false;
+  } else {
+    Dbutton.classList.remove("active_1");
+  }
+};
 inputBox.onkeyup = () => {
   let userData = inputBox.value;
   if (userData.trim() != 0) {
@@ -11,9 +27,3 @@ inputBox.onkeyup = () => {
     Dbutton.classList.remove("active_1");
   }
 };
-Dbutton.onclick = () => {
-  setTimeout(function () {
-    inputBox.value = "";
-  }, 1200);
-};
-Dbutton.disabled = true;

2c14eea7d60d841523ea68ad3f11cbc50837437e
Hung Hoang <H2PHero>
2021-11-30 12:41:58 +0700

    fixing bugs

--- static/assets/js/scroll.js
+++ static/assets/js/scroll.js
@@ -1,24 +1,19 @@
-$(function () {
-  $(window).load(function () {
-    new fullpage("#fullPage", {
-      autoScrolling: true,
-      navigation: true,
-      navigationPosition: "left",
-    });
-  });
+new fullpage("#fullPage", {
+  autoScrolling: true,
+  navigation: true,
+  navigationPosition: "left",
 });
+
 const inputBox = document.getElementById("input1");
 const Dbutton = document.getElementById("button1");
 const inputBox2 = document.getElementById("input2");
 const Dbutton2 = document.getElementById("button2");
-
 Dbutton.disabled = true;
 Dbutton.onclick = () => {
   setTimeout(function () {
     inputBox.value = "";
   }, 1300);
 };
-
 inputBox.onkeydown = () => {
   let userData = inputBox.value;
   if (userData.trim() != 0) {
@@ -31,14 +26,12 @@ inputBox.onkeyup = () => {
     Dbutton.disabled = false;
   }
 };
-
 Dbutton2.disabled = true;
 Dbutton2.onclick = () => {
   setTimeout(function () {
     inputBox2.value = "";
   }, 1300);
 };
-
 inputBox2.onkeydown = () => {
   let userData2 = inputBox2.value;
   if (userData2.trim() != 0) {
